b487d3ec47c1902a62164f767490c71478c11904
margaaustria <calumpianomargarette@gmail.com>
2026-03-27 15:14:40 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,9 @@
-let userName1 = "Elmer";
+let userName1 = "Margaux Austria";
+let age1 = 21;
+let isStudent1 = true;
+let hobby1 = ["Watching One Piece", "Reading Comics","Playing Volleyball"]
 
 console.log(typeof userName1)
+console.log(typeof age1)
+console.log(typeof isStudent1)
+console.log(typeof hobby1)
