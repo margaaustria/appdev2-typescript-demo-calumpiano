84f8cae745b0c3899949bb8f6b45986be2bd5898
margaaustria <calumpianomargarette@gmail.com>
2026-04-01 14:10:06 +0800

    Done playing and updating the 08-functions.ts

--- 08-functions.ts
+++ 08-functions.ts
@@ -1,62 +1,50 @@
 (() => {
 
-    // Function Return Value Types
-    function add(a: number, b: number): number { // you can omit the function type since typescript can infers it
-        return a + b
+    function subtract(a: number, b: number): number {
+        return a - b
     }
 
-    add(5, 10)
-    // add("5", 10)
+    console.log(subtract(20, 5)) 
 
-    // Special Type `void` - return nothing
-    // Function doesn't return a meaningful value, but it does finish
-    function log(message: string): void {
-        console.log(message)
+    function printMessage(msg: string): void {
+        console.log(`Message: ${msg}`)
     }
 
-    log("Hello, World!")
+    printMessage("Hello, Margaux!")
 
-    // Another Special Type `never`
-    // Function never finishes normally (throws error, infinite loop)
-    function logAndThrow(errorMessage: string) {
-        console.log(errorMessage)
-        throw new Error(errorMessage)
+    function fail(message: string): never {
+        throw new Error(message)
     }
 
-    // Function as Types
-    // Useful for callback functions
-    // () => {} is not same as () => void
-    // () => {} - arrow function
-    // () => void - function type
-    function performJob(cb: (m: string) => void) {  // you can also use cb: Function but () => void is the best practice
-        // ...
-        cb('Job Done!')
-    }
 
-    let logMsg = (msg: string): void => {
-        console.log(msg)
-    }
-
-    performJob(logMsg)
-
-    // Useful for objects and methods
-    type User = {
-        name: string;
-        age: number;
-        greet: () => string;
-    }
-
-    let user: User = {
-        name: "Elmer",
-        age: 39,
-        // greet: function() {
-        //     return this.name
-        // }
-        greet() {
-            console.log(`Hello, ${this.name}`)
-            return this.name
+    function repeatTask(task: (count: number) => void, times: number) {
+        for (let i = 1; i <= times; i++) {
+            task(i)
         }
     }
 
-    user.greet()
-})
+    const showCount = (n: number): void => {
+        console.log(`Count: ${n}`)
+    }
+
+    repeatTask(showCount, 3)
+
+    type Product = {
+        name: string
+        price: number
+        discount: (percent: number) => number
+    }
+
+    const product: Product = {
+        name: "Laptop",
+        price: 1000,
+        discount(percent) {
+            const discountedPrice = this.price * ((100 - percent) / 100)
+            console.log(`Discounted Price: $${discountedPrice}`)
+            return discountedPrice
+        }
+    }
+
+    product.discount(35) 
+
+})()
